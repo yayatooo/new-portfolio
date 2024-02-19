66eafed6f1df69e84b246b85e689d72d6d2ab05b
Rahmat Hidayat <yayatbx@gmail.com>
2024-02-19 20:29:05 +0800

    typo at homepage

--- app/common/home/components/HeroSection.tsx
+++ app/common/home/components/HeroSection.tsx
@@ -30,7 +30,7 @@ const HeroSection = () => {
         </p>
       </div>
       <h1 className="font-semibold bg-gradient-to-r from-blue-700 to-sky-500 bg-clip-text text-transparent py-4 xs:text-5xl lg:text-6xl">
-        Softwere Engineer
+        Software Engineer
       </h1>
       <h2 className="lg:text-2xl xs:text-lg xs:text-start text-justify">
         I&apos;m interested with the modern web development. I build app using
